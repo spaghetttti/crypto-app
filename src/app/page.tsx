d1a5e147b3fac78ec3806a200e4233c8e94fa49f
asil <asil9802mum@gmail.com>
2024-08-27 10:43:17 +0500

    style: initial ui layout created and separated into components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import AverageSidePanel from "@/components/AverageSidePanel";
+import DetailedSidePanel from "@/components/DetailedSidePanel";
 
 export default function Home() {
   return (
-    <div className="">
-      hello
+    <div className="min-h-screen bg-gray-100 p-4 flex flex-col md:flex-row">
+      <AverageSidePanel title="Average ticker values" />
+      <DetailedSidePanel />
     </div>
   );
 }
